38c4f9560c0fa12ca961feaadf7993a63f149f95
ewyluda <35318463+ewyluda@users.noreply.github.com>
2021-01-09 12:18:59 -0500

    uploading multiple points progress

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -5,21 +5,19 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Get data from cities.js
+let cityData = cities;
 
-// Add a cirle marker to the map (radius = 100m)
-// L.circle([34.0522, -118.2437], {
-//    radius: 100
-// }).addTo(map);
-
-// Add a cirle marker to the map using pixel radius L.circleMarker method.
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black',
-    fillColor: '#ffffa1'
-}).addTo(map);
-
+// Iterate/Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: 'orange'
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
