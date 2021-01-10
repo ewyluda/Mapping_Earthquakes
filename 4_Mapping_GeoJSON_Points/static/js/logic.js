de99e6a92d6e36d13ffb0dd344b3b6822c592159
ewyluda <35318463+ewyluda@users.noreply.github.com>
2021-01-10 09:51:30 -0500

    updating file structure

--- 4_Mapping_GeoJSON_Points/static/js/logic.js
+++ 4_Mapping_GeoJSON_Points/static/js/logic.js
@@ -34,7 +34,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/ewyluda/Mapping_Earthquakes/main/Mapping_GeoJSON_Points/static/data/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/ewyluda/Mapping_Earthquakes/main/data/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
